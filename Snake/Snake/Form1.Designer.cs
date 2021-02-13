2b9ac9d898652c37d2fbed0df8fa5eac98bf0c73
Artem <74727214+VantyArtem@users.noreply.github.com>
2021-02-14 02:22:54 +0900

    Add files via upload

---
+++ Snake/Snake/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(560, 518);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 26);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label4.Location = new System.Drawing.Point(539, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "X";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.ClientSize = new System.Drawing.Size(584, 597);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Snake/Snake/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        void Update(Bitmap map,Color clr)
+        {
+            Graphics graphics = null;
+            try
+            {
+                graphics = Graphics.FromImage(map);
+            }
+            catch { }
+            graphics.Clear(clr);
+
+        }
+        void FillColor(Bitmap bit, Color color)
+        
+        {
+            for (int l = 0; l < bit.Width; l++)
+            {
+                for (int i = 0; i < bit.Height; i++)
+                {
+                    bit.SetPixel(i, l, color);
+                }
+            }
+        }
+        void Game_Over()
+        {
+            MessageBox.Show($"Вы проиграли                  \n                            Ваш Счёт: {och} ", "Проигрыш!", MessageBoxButtons.OK);
+            foreach (var s in Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName))
+            {
+                s.Kill();
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(delegate () { Play(); });
+            thread.Start();
+            Thread Th = new Thread(delegate () { Contr(); });
+            Th.Start();
+        }
+        string but = "";
+        short och = 0;
+        void Play()
+        {
+            Random random = new Random();
+            int width = 300;
+            int height = 300;
+            Bitmap bitmap = new Bitmap(width, height);
+            int EatX = Convert.ToInt32(random.Next(1, bitmap.Height/10).ToString()+ "0");
+            int EatY = Convert.ToInt32(random.Next(1, bitmap.Width/10).ToString()+ "0");
+            int PlayerX = height/2;
+            int PlayerY = width;
+            List<string> list = new List<string>();
+            list.Add($"{PlayerX};{PlayerY}");
+            list.Add($"{PlayerX};{PlayerY+1}");
+            list.Add($"{PlayerX};{PlayerY + 2}");
+            FillColor(bitmap, Color.FromArgb(100, 0, 0, 0));
+            short axisX = 0;
+            short axisY = 0;
+            but = "W";
+            for (; ; )
+            {
+                Bitmap Set(int x, int y, Bitmap bit, Color clr, int size)
+                {
+                    x /= size;
+                    y /= size;
+                    x *= size;
+                    y *= size;
+                    if (x > size || y > size)
+                    {
+                        for (int k = x - size; k < x; k++)
+                        {
+                            for (int m = y - size; m < y; m++)
+                            {
+                                try { bit.SetPixel(k, m, clr); } catch { }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int k = x - size; k < x; k++)
+                        {
+                            for (int m = y - size; m < y; m++)
+                            {
+                                bit.SetPixel(k, m, clr);
+                            }
+                        }
+                    }
+
+                    return bit;
+                }
+                void Eat()
+                {
+                    och++;
+                    list.Add($"{list[list.Count - 1].Split(';')[0]};{list[list.Count - 1].Split(';')[1]}");
+                    EatX = Convert.ToInt32(random.Next(1, bitmap.Height / 10).ToString() + "0");
+                    EatY = Convert.ToInt32(random.Next(1, bitmap.Width / 10).ToString() + "0");
+                }
+                switch (but)
+                {
+                    case "W":
+                        PlayerY -= 10;
+                        axisY++;
+                        axisX = 0;
+                        break;
+                    case "A":
+                        PlayerX -= 10;
+                        axisX++;
+                        axisY = 0;
+                        break;
+                    case "S":
+                        PlayerY += 10;
+                        axisY++;
+                        axisX = 0;
+                        break;
+                    case "D":
+                        PlayerX += 10;
+                        axisX++;
+                        axisY = 0;
+                        break;
+                }
+                list.Remove(list[0]);
+                list.Add($"{PlayerX};{PlayerY}");
+                Update(bitmap, Color.FromArgb(100, 0, 0, 0));
+                //Яблоко
+                Set(EatX, EatY, bitmap, Color.FromArgb(255, 255, 100, 100), 10);
+                //Змейка
+                int s = 0;
+                foreach (var u in list)
+                {
+                    if (s < 150)
+                    {
+                        s += 3;
+                    }
+                    Set(Convert.ToInt32(u.Split(';')[0]), Convert.ToInt32(u.Split(';')[1]), bitmap, Color.FromArgb(255, 0, 100 + s, 0), 10);
+                }
+                if (PlayerX > height || PlayerY > width || PlayerX < 10 || PlayerY < 10)
+                {
+                    Game_Over();
+                }
+                //boom
+                for (int i = 0; i < list.Count - 2; i++)
+                {
+                    if (list[i] == list[list.Count - 1])
+                    {
+                        Game_Over();
+                    }
+                }
+                //Display
+                pictureBox1.Image = bitmap;
+                if (PlayerX > height || PlayerY > width || PlayerX < 10 || PlayerY < 10)
+                {
+                    Game_Over();
+                }
+                //Eat
+                if (PlayerX == EatX && PlayerY == EatY)
+                {
+                    Eat();
+                }
+                try { this.Invoke((MethodInvoker)delegate () { label3.Text = $"Очки: {och}"; }); } catch { }
+                Thread.Sleep(130);
+            }
+        }
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+        void Contr()
+        {
+            for (; ; )
+            {
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Thread.Sleep(1);
+                    for (Int32 i = 0; i < 255; i++)
+                    {
+
+                        int keys_ = GetAsyncKeyState(i);
+                        if (keys_ == 1 || keys_ == -32767)
+                        {
+                            if ((Keys)i == Keys.W)
+                            {
+                                if (but == "S")
+                                {
+
+                                }
+                                else
+                                {
+                                    but = "W";
+                                }
+                            }
+                            if ((Keys)i == Keys.A)
+                            {
+                                if (but == "D")
+                                {
+
+                                }
+                                else
+                                {
+                                    but = "A";
+                                }
+                            }
+                            if ((Keys)i == Keys.S)
+                            {
+                                if (but == "W")
+                                {
+
+                                }
+                                else
+                                {
+                                    but = "S";
+                                }
+                            }
+                            if ((Keys)i == Keys.D)
+                            {
+                                if (but == "A")
+                                {
+
+                                }
+                                else
+                                {
+                                    but = "D";
+                                }
+                            }
+                            Thread.Sleep(100);
+                        }
+                    }
+                }
+            }
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+            foreach (var s in Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName))
+            {
+                s.Kill();
+            }
+        }
+    }   
+}
